e67d65942049a3d2d8ac9c8ee25758898614ca03
Mauri <mauricio.costa@simplex.software>
2022-02-23 10:49:33 -0300

    improve db .sql script.

--- listener/sql/bcbrawlers.sql
+++ listener/sql/bcbrawlers.sql
@@ -1,31 +1,33 @@
+DROP TYPE IF EXISTS bcbrawlers_actions CASCADE;
 CREATE TYPE bcbrawlers_actions AS ENUM (
-  'gearadd'   
-  'gearrem'    
-  'brawleradd'    
-  'brawlerrem'    
-  'ringselect'    
-  'brawl'    
-  'receiverand'      
-  'heal'    
-  'wdrawbrawler'    
-  'craft'    
-  'wdrawbrwl'    
-  'configset'    
-  'configclear'      
-  'maintset'    
-  'brawlerset'    
-  'gearset'    
-  'ringset'    
-  'ringdset'    
-  'slotdset'    
-  'brawlerdel'    
-  'geardel'    
-  'ringdel'    
-  'ringddel'    
-  'slotddel'    
+  'gearadd',   
+  'gearrem',    
+  'brawleradd',    
+  'brawlerrem',    
+  'ringselect',    
+  'brawl',    
+  'receiverand',      
+  'heal',    
+  'wdrawbrawler',    
+  'craft',    
+  'wdrawbrwl',    
+  'configset',    
+  'configclear',      
+  'maintset',    
+  'brawlerset',    
+  'gearset',    
+  'ringset',    
+  'ringdset',    
+  'slotdset',    
+  'brawlerdel',    
+  'geardel',    
+  'ringdel',    
+  'ringddel',    
+  'slotddel',    
   'recset'    
 );
 
+DROP TABLE IF EXISTS public.bcbrawlers;
 CREATE TABLE public.bcbrawlers (
         id serial NOT NULL,
         action bcbrawlers_actions,
@@ -33,7 +35,7 @@ CREATE TABLE public.bcbrawlers (
         gear_id bigint,
         slot_id bigint,
         ring_id bigint,
-        owner text NOT NULL,  
+        owner text NULL,  
         extradata jsonb NULL,
         executiontime timestamp NOT NULL,
         blocknumber bigint,
